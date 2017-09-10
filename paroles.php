af32f476059d1806e3412afb684afec127cb5298
Guillaume MELLET <guillaume.mellet@ekino.com>
2017-09-10 02:03:05 +0200

    initialisation

---
+++ paroles.php
@@ -0,0 +1,129 @@
+
+<?php include('backend/insertMail.php'); ?>
+
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+
+<title>Paroles - Les Conteuses de Pas</title>
+<meta charset="utf-8">
+<meta name="title" content="Paroles - les Conteuses de Pas">
+<meta name="description" content="Groupe de musique et de chant tradtionnels composé de 4 femmes. Elles se produisent en Bal etpropose également des Stage de danse.">
+<meta name="author" content="Guillaume MELLET">	
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<script src="https://use.fontawesome.com/325310aa84.js"></script>
+<script src="script/jquery-3.2.1.min.js" type="text/javascript" charset="utf-8"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+</head>
+<body id="paroles">
+
+	<section id="global-container">
+
+		<span class="sLeft"><a href="/CDP/index.php" class="green small">< Retour à l'accueil</a></span>
+
+		<div id="fond-logo-titre" class="clear"><span id="logo-titre"></span></div>
+
+		<h1>Nos Paroles</h1>
+	
+		<a download href="paroles/1-ah-si-tu-amusons-nous.pdf" class="paroles orange">
+			<span class="nbrs">01</span>
+			<span class="pInfo">
+				<h2>Ah si tu m'abandonnes - Amusons nous fillette</h2>
+				<h3 class="left">Bourée à 3 temps</h3>
+				<h4 class="right">04:21</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/2-une-fillette-pelote.pdf" class="paroles blue">
+			<span class="nbrs">02</span>
+			<span class="pInfo">
+				<h2>Une fille à 14 ans - La pelote à Marion</h2>
+				<h3 class="left">Valse à 3 temps</h3>
+				<h4 class="right">04:21</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/4-par-derriere.pdf" class="paroles green">
+			<span class="nbrs">04</span>
+			<span class="pInfo">
+				<h2 class="uL">Par derrière notre maison</h2>
+				<h3 class="left">Ronde à 3 pas du parys de Caux</h3>
+				<h4 class="right">03:41</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/5-cueillir-la-rose.pdf" class="paroles pink">
+			<span class="nbrs">05</span>
+			<span class="pInfo">
+				<h2 class="uL">Cueillir la rose</h2>
+				<h3 class="left">Mazurka</h3>
+				<h4 class="right">05:31</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/6-dans-paris.pdf" class="paroles orange">
+			<span class="nbrs">06</span>
+			<span class="pInfo">
+				<h2 class="uL">Dans Paris</h2>
+				<h3 class="left">Scottish</h3>
+				<h4 class="right">05:14</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/7-de-bon-matin.pdf" class="paroles blue">
+			<span class="nbrs">07</span>
+			<span class="pInfo">
+				<h2 class="uL">De bon matin</h2>
+				<h3 class="left">Rond d'Argenton</h3>
+				<h4 class="right">04:49</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/9-derriere-de-chez.pdf" class="paroles green">
+			<span class="nbrs">09</span>
+			<span class="pInfo">
+				<h2 class="uL">Derrière chez mon père</h2>
+				<h3 class="left">Polka</h3>
+				<h4 class="right">04:39</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/11-se-ieu.pdf" class="paroles pink">
+			<span class="nbrs">11</span>
+			<span class="pInfo">
+				<h2 class="uL">Se ieu sabia</h2>
+				<h3 class="left">Valse à 3 temps</h3>
+				<h4 class="right">04:35</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/12-mon-pere-il-veut.pdf" class="paroles orange">
+			<span class="nbrs">12</span>
+			<span class="pInfo">
+				<h2>Mon père il veut m'y marier</h2>
+				<h3 class="left">Pilé menu</h3>
+				<h4 class="right">05:32</h4>
+			</span>
+		</a>
+
+		<a download href="paroles/13-a-la-claire.pdf" class="paroles blue">
+			<span class="nbrs">13</span>
+			<span class="pInfo">
+				<h2 class="uL">&Agrave; la claire fontaine</h2>
+				<h3 class="left">Hanter dro</h3>
+				<h4 class="right">03:58</h4>
+			</span>
+		</a>
+
+		<span class="sLeft"><a href="/CDP/index.php" class="green small">< Retour à l'accueil</a></span>
+		
+	</section>
+
+</body>
+</html>
