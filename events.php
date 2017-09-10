8d7529da35649b06652bc34adc6d95e5fe61cbd0
Guillaume MELLET <guillaume.mellet@ekino.com>
2017-09-10 12:31:10 +0200

    Mise en place navigation et footer

---
+++ events.php
@@ -0,0 +1,54 @@
+
+<?php include('backend/insertMail.php'); ?>
+
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+
+<title>Paroles - Les Conteuses de Pas</title>
+<meta charset="utf-8">
+<meta name="title" content="Paroles - les Conteuses de Pas">
+<meta name="description" content="Groupe de musique et de chant tradtionnels composé de 4 femmes. Elles se produisent en Bal etpropose également des Stage de danse.">
+<meta name="author" content="Guillaume MELLET">	
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<script src="https://use.fontawesome.com/325310aa84.js"></script>
+<script src="script/jquery-3.2.1.min.js" type="text/javascript" charset="utf-8"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+</head>
+<body id="paroles">
+
+	<nav id="navigation">
+		<a id="logo" target="_blank" href="/CDP/index.php"><img src="" alt="logo abrégé conteuses"></a>
+		<ul>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+		</ul>
+		<div id="player"></div>
+	</nav>
+
+	<section id="global-container">
+
+		
+
+		<span class="sLeft"><a href="/CDP/index.php" class="green small">< Retour à l'accueil</a></span>
+		
+	</section>
+	
+	<footer>
+		<span id="copyright"><? echo date("Y"); ?> - Les Conteuses de Pas</span>
+		<span id="footer-nav">
+			<a href="mailto:contact@leconteusesdepas.fr">Contactez-nous</a>			
+		</span>		
+	</footer>
+
+</body>
+</html>
