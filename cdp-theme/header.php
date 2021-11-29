794dbd50444e79571308a8a113b25f897e00cbf6
Sebastien Picq <sebastien.picq@ekino.com>
2021-11-29 10:30:22 +0100

    feat(lyric): dynamize lyrics

--- cdp-theme/header.php
+++ cdp-theme/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="<?php echo get_template_directory_uri();?>/css/images/CDP_favicon.ico" />
 
     <!-- TODO: still used? -->
